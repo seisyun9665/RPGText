bff90b426506f4394eac5b7076253f0719f8af94
seisyun9665 <58073880+seisyun9665@users.noreply.github.com>
2021-11-19 20:10:13 +0900

    CustomScoreのスコア保存形式を変更

--- src/main/java/seisyun/rpgtext/CustomScore.java
+++ src/main/java/seisyun/rpgtext/CustomScore.java
@@ -1,5 +1,4 @@
 package seisyun.rpgtext;
-
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -7,7 +6,7 @@ import org.bukkit.plugin.Plugin;
 public class CustomScore {
     // プラグイン独自のスコアボードを作成する。
     // プラグインフォルダ内にscoreboard.ymlを作成してプレイヤーごとに変数を設定する
-    // CustomConfigが必要
+    // 別のクラスCustomConfigが必要
     private CustomConfig customConfig;
     private FileConfiguration config;
     private static final String DEFAULT_SECTION_NAME = "score";
@@ -24,29 +23,28 @@ public class CustomScore {
 
     // プレイヤーの特定スコアを取得する
     public int get(String scoreName, Player player){
-        return config.getInt(DEFAULT_SECTION_NAME+"."+scoreName + "." + player.getUniqueId(),0);
+        return config.getInt(DEFAULT_SECTION_NAME+"."+player.getUniqueId() + "." + scoreName,0);
     }
 
     // プレイヤーの特定スコアを設定する
     public void set(String scoreName,Player player,int value){
-        config.set(DEFAULT_SECTION_NAME+"."+scoreName + "." + player.getUniqueId(),value);
+        config.set(DEFAULT_SECTION_NAME+"."+player.getUniqueId() + "." + scoreName,value);
         customConfig.saveConfig();
         reload();
     }
 
-    // プレイヤーの全てのスコアを0にする
+    // 特定プレイヤーの全てのスコアを0にする
     public void resetPlayer(Player player) {
         for (String key : config.getConfigurationSection(DEFAULT_SECTION_NAME).getKeys(false)) {
-            config.set(DEFAULT_SECTION_NAME+"."+key+"."+player.getUniqueId(),0);
+            config.set(DEFAULT_SECTION_NAME+"."+player.getUniqueId(), null);
         }
         customConfig.saveConfig();
         reload();
     }
 
     // スコアを読み込みなおす（手動で変更した場合など用）
-    void reload(){
+    public void reload(){
         customConfig.reloadConfig();
         config = customConfig.getConfig();
     }
-
 }
